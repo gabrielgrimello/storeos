ef16fbae0501fda0a514a4bb2e4c4c7683149e93
gabri <gabri@GABRIEL>
2020-04-13 08:16:00 -0300

    removi email e telegram

--- application/config/telegram.php
+++ application/config/telegram.php
@@ -4,5 +4,5 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-$config['bot_token'] = '833254490:AAE4g6o1lRMQ3t0JRoBxiKuUcBmRjo4Sg1k';
-$config['chat_id'] = '871332611';
+$config['bot_token'] = '';
+$config['chat_id'] = '';
